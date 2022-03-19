64d3a2f361c000a81df3adf76457476d660a3e07
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 18:21:44 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double salario, fgts;
+
+Console.Write("Insira seu salário (R$): ");
+salario = Convert.ToDouble(Console.ReadLine()!);
+
+fgts = (salario * 8) / 100;
+
+Console.WriteLine($"\nFGTS: {fgts:C}");
